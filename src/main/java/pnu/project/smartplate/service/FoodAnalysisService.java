e8b7f538f0375f5c0b7c093bc05c40a08b024754
Kim Sanghae <pelikan@pusan.ac.kr>
2024-09-20 21:58:21 +0900

    fix($service): 음식 클래스 번호 리스트 지역 변수로 변경

--- src/main/java/pnu/project/smartplate/service/FoodAnalysisService.java
+++ src/main/java/pnu/project/smartplate/service/FoodAnalysisService.java
@@ -7,21 +7,18 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
@@ -47,9 +44,6 @@ public class FoodAnalysisService {
     @Value("${file.upload-dir}")
     private String uploadDir;
 
-    @Value("${fastapi.url}")
-    private String fastApiUrl;
-
     @Value("${fastapi.port}")
     private String fastApiPort;
 
@@ -58,9 +52,6 @@ public class FoodAnalysisService {
 
     private final RestTemplate restTemplate;
 
-    private Map<String, FoodInfo> savedResults = new HashMap<>();
-
-    Logger logger = LoggerFactory.getLogger(FoodAnalysisService.class);
 
     @Value("${fastapi.scheme}")
     private String fastApiScheme;
@@ -68,116 +59,114 @@ public class FoodAnalysisService {
     @Value("${fastapi.host}")
     private String fastApiHost;
 
-
     @Autowired
     public FoodAnalysisService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
-    public void saveResult(String customType, String customAmount, String imageUrl) {
-        String dateKey = LocalDateTime.now().toString();
-        savedResults.put(dateKey, new FoodInfo(customType, customAmount, imageUrl));
-    }
-
-    public Map<String, FoodNutrient> analyzeImage(String imagePath) throws IOException {
-
-        // 헤더
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-        String fullUrl = UriComponentsBuilder.newInstance()
-            .scheme(fastApiScheme)
-            .host(fastApiHost)
-            .port(fastApiPort)
-            .path(fastApiEndpoint)
-            .toUriString();
-
-        // 바디
-        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-
-        Path path = Paths.get(uploadDir + imagePath);
-        FileSystemResource fileResource = new FileSystemResource(path.toFile());
-
-        body.add("file", fileResource);
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
-
-        // 요청
-        log.info("requestEntity = " + requestEntity);
+    public Map<String, FoodNutrient> analyzeImage(String imagePath) throws IOException { // 이미지 분석
+        String fullUrl = getFullUrlString(); // FastAPI 주소 가져오기
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = getResult(
+            imagePath); // RequestEntity
         ResponseEntity<String> response = restTemplate.postForEntity(fullUrl, requestEntity,
-            String.class);
-
-        log.info(response.toString());
-
+            String.class); // ResponseEntity
         ObjectMapper mapper = new ObjectMapper();
         List<Map<String, Object>> dataList = mapper.readValue(response.getBody(),
-            new TypeReference<List<Map<String, Object>>>() {
+            new TypeReference<>() {
             });
 
         List<Integer> classes = new ArrayList<>();
         List<Double> confidences = new ArrayList<>();
         List<Double> amounts = new ArrayList<>();
 
-        for (Map<String, Object> item : dataList) {
-            classes.add((Integer) item.get("class"));
-            confidences.add((Double) item.get("confidence"));
-            amounts.add((Double) item.get("amount"));
-        }
+        addDataToList(dataList, classes, confidences, amounts);
+        log.info(classes.toString());
+        return getStringFoodNutrientMap(response, classes, amounts);
+    }
 
+    private String getFullUrlString() { // FastAPI 주소 Get
+        return UriComponentsBuilder.newInstance().scheme(fastApiScheme).host(fastApiHost)
+            .port(fastApiPort).path(fastApiEndpoint).toUriString();
+    }
 
-        // 리턴
+    private Map<String, FoodNutrient> getStringFoodNutrientMap(ResponseEntity<String> response,
+        List<Integer> classes, List<Double> amounts) throws IOException {
         if (response.getStatusCode() == HttpStatus.OK) {
-            ArrayList<FoodInfo> foodInfos = new ArrayList<>();
-            for (int i = 0; i < classes.size(); i++) {
-                String foodName = FoodClass.values()[classes.get(i)].getKrName();
-                foodInfos.add(new FoodInfo(foodName, amounts.get(i).toString()));
-            }
-
-            // TODO: FastAPI에서 응답을 Map 형식으로 정제하는 과정 구현 -(gimsanghae, 2024-08-16, 18:27)
-            Map<String, Integer> foods = new LinkedHashMap<>();
-            for (int i = 0; i < foodInfos.size(); i++) {
-                foods.put(foodInfos.get(i).getFoodName(),
-                    (int) Math.round(Double.parseDouble(foodInfos.get(i).getFoodAmount())));
-            }
-            System.out.println(foods.size());
-            for (String s : foods.keySet()) {
-                System.out.println("s - ");
-                System.out.println(s);
-                System.out.println(foods.get(s));
-            }
-
-            // CSV에서 해당 음식의 정보 가져오기
-            Map<String, FoodNutrient> foodMap = findFood(foods);
-            for (String foodName : foodMap.keySet()) {
-                log.info(foodMap.get(foodName).toString());
-            }
-            return foodMap;
+            ArrayList<FoodInfo> foodInfos = getFoodInfos(classes, amounts);
+            Map<String, Integer> foods = getStringIntegerMap(foodInfos);
+            return getStringFoodNutrientMap(foods); // CSV에서 해당 음식의 정보 가져오기
         } else {
             throw new RuntimeException(
                 "Failed to analyze image. Status code: " + response.getStatusCode());
         }
     }
 
-    public String saveImg(MultipartFile imageFile) throws IOException, InterruptedException {
+    private Map<String, FoodNutrient> getStringFoodNutrientMap(Map<String, Integer> foods)
+        throws IOException {
+        Map<String, FoodNutrient> foodMap = findFood(foods);
+        for (String foodName : foodMap.keySet()) {
+            log.info(foodMap.get(foodName).toString());
+        }
+        return foodMap;
+    }
+
+    private static Map<String, Integer> getStringIntegerMap(ArrayList<FoodInfo> foodInfos) {
+        Map<String, Integer> foods = new LinkedHashMap<>();
+        for (FoodInfo foodInfo : foodInfos) {
+            foods.put(foodInfo.getFoodName(),
+                (int) Math.round(Double.parseDouble(foodInfo.getFoodAmount())));
+        }
+        return foods;
+    }
+
+    private ArrayList<FoodInfo> getFoodInfos(List<Integer> classes, List<Double> amounts) {
+        ArrayList<FoodInfo> foodInfos = new ArrayList<>();
+        for (int i = 0; i < classes.size(); i++) {
+            String foodName = FoodClass.values()[classes.get(i)].getKrName();
+            foodInfos.add(new FoodInfo(foodName, amounts.get(i).toString()));
+        }
+        return foodInfos;
+    }
+
+    private static void addDataToList(List<Map<String, Object>> dataList, List<Integer> classes,
+        List<Double> confidences, List<Double> amounts) {
+        for (Map<String, Object> item : dataList) {
+            classes.add((Integer) item.get("class"));
+            confidences.add((Double) item.get("confidence"));
+            amounts.add((Double) item.get("amount"));
+        }
+    }
+
+    private HttpEntity<MultiValueMap<String, Object>> getResult(
+        String imagePath) { // Python과 Http 통신
+        // 헤더
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        // 바디
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        Path path = Paths.get(uploadDir + imagePath);
+        FileSystemResource fileResource = new FileSystemResource(path.toFile());
+        body.add("file", fileResource);
+        return new HttpEntity<>(body, headers);
+    }
+
+    public String saveImg(MultipartFile imageFile) throws IOException {
         String originalFileName = imageFile.getOriginalFilename();
         String extension =
             originalFileName != null ? originalFileName.substring(originalFileName.lastIndexOf("."))
                 : "";
         String fileName = UUID.randomUUID() + extension;
-
         String filePath = uploadDir + fileName;
-
         Path path = Paths.get(filePath);
-
         Files.createDirectories(path.getParent());
         Files.copy(imageFile.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
-
         return fileName;
     }
 
-    // TODO: 12가지 음식 영양 정보 CSV 파일 읽어서 LIST형식으로 리턴 -(gimsanghae, 2024-08-16, 18:28)
     private Map<String, FoodNutrient> readCSV() throws IOException {
         File file = new File("FoodNutrient.csv");
         BufferedReader bufferedReader = new BufferedReader(
-            new InputStreamReader(new FileInputStream(file), "UTF-8"));
+            new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
         String line;
         // foods, <음식명, 음식 영양 정보>
         Map<String, FoodNutrient> foods = new LinkedHashMap<>();
@@ -186,7 +175,6 @@ public class FoodAnalysisService {
             // 제한 없이 split을 한다 -> 구분자가 나올면 무제한 분할한다.
             // positive lookahaed (?=...)
             String[] split = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-
             String name = split[0];
             double amount = Double.parseDouble(split[1]);
             double calory = Double.parseDouble(split[2]);
@@ -194,7 +182,6 @@ public class FoodAnalysisService {
             double protein = Double.parseDouble(split[6]);
             double fat = Double.parseDouble(split[5]);
             double sugar = Double.parseDouble(split[4]);
-
             // 음식 영양 정보 객체 생성
             FoodNutrient nutrient = new FoodNutrient(name, amount, calory, carbo, protein, fat,
                 sugar);
@@ -203,22 +190,17 @@ public class FoodAnalysisService {
         return foods;
     }
 
-    // TODO(gimsanghae, 2024-08-15, 목, 22:09): 전달받은 음식명과 중량 데이터로 영양 정보 CSV에서 해당 음식 영양 정보 찾기
     private Map<String, FoodNutrient> findFood(Map<String, Integer> foodInfos) throws IOException {
         Map<String, FoodNutrient> foodNutrientMap = readCSV();
         return foodInfos.entrySet().stream()
             .filter(entry -> foodNutrientMap.containsKey(entry.getKey()))
-            .collect(Collectors.toMap(
-                Map.Entry::getKey,
-                entry -> {
-                    FoodNutrient nutrient = foodNutrientMap.get(entry.getKey());
-                    calculateNutrient(nutrient, entry.getValue());
-                    return nutrient;
-                }
-            ));
+            .collect(Collectors.toMap(Map.Entry::getKey, entry -> {
+                FoodNutrient nutrient = foodNutrientMap.get(entry.getKey());
+                calculateNutrient(nutrient, entry.getValue());
+                return nutrient;
+            }));
     }
 
-    // TODO(gimsanghae, 2024-08-15, 목, 22:13): 인식된 음식의 중량을 바탕으로 영양 정보 계산하기
     private void calculateNutrient(FoodNutrient foodNutrient, Integer amount) {
         double amountRatio = amount / foodNutrient.getAmount(); // CSV에 기재된 중량과 인식된 음식의 중량 비율
         foodNutrient.setCalory(Math.round(foodNutrient.getCalory() * amountRatio));
